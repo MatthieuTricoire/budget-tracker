dd07c05a1b1427a932d0d3c7651d07c012933728
Matthieu Tricoire <matthieutricoire@Matthieus-MacBook-Pro.local>
2024-04-30 15:11:38 +0100

    define navbar links and labels

---
+++ data/navbar.ts
@@ -0,0 +1,14 @@
+export const navLinks = [
+  {
+    label: "Dashboard",
+    link: "/",
+  },
+  {
+    label: "Transaction",
+    link: "/transactions",
+  },
+  {
+    label: "Manage",
+    link: "/manage",
+  },
+];
