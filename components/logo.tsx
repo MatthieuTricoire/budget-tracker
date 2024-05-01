e41638120b75125ff9b116050b59f9ec54b73cea
Matthieu Tricoire <matthieutricoire@Matthieus-MacBook-Pro.local>
2024-05-01 17:12:55 +0100

    update color logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -5,8 +5,8 @@ import React from "react";
 export const Logo = () => {
   return (
     <Link href="/" className="flex items-center gap-2">
-      <PiggyBankIcon className="stroke size-11 stroke-blue-600" />
-      <span className="bg-gradient-to-r from-blue-300 to-cyan-600 bg-clip-text text-3xl/tight tracking-tighter text-transparent font-bold">
+      <PiggyBankIcon className="stroke size-11 stroke-yellow-200" />
+      <span className="bg-gradient-to-r from-yellow-200 to-amber-600 bg-clip-text text-3xl/tight tracking-tighter text-transparent font-bold">
         Budget Tracker
       </span>
     </Link>
@@ -16,7 +16,7 @@ export const Logo = () => {
 export const MobileLogo = () => {
   return (
     <Link href="/" className="flex items-center gap-2">
-      <span className="bg-gradient-to-r from-blue-300 to-cyan-600 bg-clip-text text-xl/tight tracking-tighter text-transparent font-bold">
+      <span className="bg-gradient-to-r from-yellow-200 to-amber-600 bg-clip-text text-xl/tight tracking-tighter text-transparent font-bold">
         Budget Tracker
       </span>
     </Link>
